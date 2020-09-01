f59e5fb4da29a8bf355925f51a30dfd5a9b2d8d6
Duamelo <franckdosseh17@gmail.com>
2020-09-01 18:55:33 +0200

    :tada: initial Commit!

---
+++ client/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <div class="jumbotron">
+      <h1 class="display-3">Welcome to IMSP-Forum</h1>
+      <p class="lead">A fully open source forum made with heart by Duamel.</p>
+      <hr class="my-4">
+      <p>This forum is all about physics, mathematics and informatics at IMSP
+        <br>
+        So be kind and careful !
+      </p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Sign Up</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+};
+</script>
